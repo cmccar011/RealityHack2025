a2a0ede4194993122e4e6ef39d0d3e6f3a44fe1b
GamingGabriel <gabrieljrosss@gmail.com>
2025-01-25 17:24:55 -0500

    Smokey Campire
    We have a campfire that emits smokes on a random timer.

---
+++ Reality Hack 2025!/Assets/flammableScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flammableScript : MonoBehaviour
+{
+
+    public bool onFire = false;
+    public float igniteCount = 0;
+    public int igniteRate = 1;
+
+    public ParticleSystem particleSys;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        igniteCount +=  igniteRate * Time.deltaTime;
+        //Debug.Log(igniteCount);
+        if (igniteCount >= 4)
+        {
+            IgniteCheck();
+            igniteCount = 0;
+            Debug.Log(onFire);
+        }
+    }
+
+    public void IgniteCheck()
+    {
+       int igniteNum = Random.Range(0, 3);
+       Debug.Log(igniteNum);
+       //Debug.Log(igniteNum); 
+       if (igniteNum == 0)
+       {
+        Ignite();
+       }
+    }
+
+    public void Ignite()
+    {  
+        Debug.Log("Ignition Start");
+        onFire = true;
+        particleSys.Play();
+        Debug.Log("Ignition End");
+    }
+}
